31a476214b3d5e8323f303dc6a0befc58ee4704a
miguelseguias <migan2000@gmail.com>
2025-03-13 01:17:50 -0400

    fg

--- routes/recipeRoutes.js
+++ routes/recipeRoutes.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { config } from './config.js';
+import { config } from '../config.js';
 
 export const createRecipeRoutes = (app) => {
   app.get('get-recipes', async (req, res) => {
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { config } from './config.js';
+import { config } from '../config.js';
 
 export const createUserRoutes = (app) => {
   // app.get('/test-db', async (req, resp) => {
