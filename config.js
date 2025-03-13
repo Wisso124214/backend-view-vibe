803e83aacc423d82b9ccf3c6a67ca26643be5008
miguelseguias <migan2000@gmail.com>
2025-03-13 01:08:54 -0400

    gh

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 export const config = {
   DB_URL: 'mongodb+srv://Usuario11:user11@cluster01.rbzr4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster01',
   PORT: process.env.PORT || 3000,
-  SERVER_IP: '192.168.100.80',
+  SERVER_IP: '192.168.100.1',
   PROTOCOL: 'http',
 }
 
