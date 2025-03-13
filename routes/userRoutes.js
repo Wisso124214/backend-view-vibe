bef1ff11714d5e7eb708a6cc56977e302e0ffbd0
Wisso124214 <luisdavidbustosnunez@gmail.com>
2025-03-13 00:24:34 -0400

    imports

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { config } from './config.js';
+import { config } from './config.js';
 
 export const createUserRoutes = (app) => {
   // app.get('/test-db', async (req, resp) => {
