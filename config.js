8933134f65ac427cc9ea98cc8fb87f0852dbeab6
Wisso124214 <luisdavidbustosnunez@gmail.com>
2025-03-13 01:02:34 -0400

    edited

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
 export const config = {
-    DB_URL: 'mongodb+srv://Usuario11:user11@cluster01.rbzr4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster01',
-    PORT: process.env.PORT || 3000,
-    SERVER_IP: '192.168.100.80',
-    PROTOCOL: 'http',
-  
-  }
-  
-  export const SERVER_URL = `${config.PROTOCOL}://${config.SERVER_IP}:${config.PORT}`;
+  DB_URL: 'mongodb+srv://Usuario11:user11@cluster01.rbzr4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster01',
+  PORT: process.env.PORT || 3000,
+  SERVER_IP: '192.168.100.80',
+  PROTOCOL: 'http',
+}
+
+export const SERVER_URL = `${config.PROTOCOL}://${config.SERVER_IP}:${config.PORT}`;
+
+//edited
